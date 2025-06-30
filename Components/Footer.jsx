1b82759b35b33f3be5fa5d0cf4a385e622bc017b
Niroj Thapa <155825050+Niroj-t@users.noreply.github.com>
2025-06-30 15:18:52 +0545

    Update Footer.jsx

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 bg-gray-900 text-center text-gray-300 text-sm border-t border-gray-200 card shadow-lg">
       <div>
-         Built with <span className="font-semibold text-green-600">Next.js</span>, <span className="font-semibold text-green-600">Tailwind CSS</span> &middot; Coded in <span className="font-semibold text-green-600">Cursor</span> and deployed with <span className="font-semibold text-green-600">Vercel</span>
+         Built with <span className="font-semibold text-green-600">Next.js</span>, <span className="font-semibold text-green-600">Tailwind CSS</span> &middot; Coded in <span className="font-semibold text-green-600">VS Code</span> and deployed with <span className="font-semibold text-green-600">Vercel</span>
       </div>
       <div className="mt-1">
         Developed by <Link href="https://github.com/Niroj-t" className="text-green-600 hover:underline" target="_blank">Niroj Thapa</Link>
